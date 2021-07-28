86e66e72531b945d0f6b1deda49d186f7ca43313
FacuVerge <facu.verge@gmail.com>
2021-07-28 18:51:46 -0300

    Aplico patrón Singleton y agrego superAdministradores

--- src/main/java/Business/Cadena.java
+++ src/main/java/Business/Cadena.java
@@ -1,7 +1,10 @@
 package Business;
 
+import Security.SuperAdministrador;
 import Security.Usuario;
 import lombok.Data;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -10,8 +13,29 @@ public class Cadena {
     private List<Cine> cines;
     private List<Pelicula> peliculas;
     private List<Usuario> usuarios;
+    private List<SuperAdministrador> superAdministradores;
     private List<Producto> productos;
     private Planificador planificacionActual;
     private int porcentajeGanancia;
+
+    //SINGLETON
+
+    private static Cadena instance = null;
+
+    private Cadena() {}
+
+    public static Cadena getInstance() {
+        if (instance == null) {
+            instance = new Cadena();
+            instance.cines = new ArrayList<>();
+            instance.peliculas = new ArrayList<>();
+            instance.usuarios = new ArrayList<>();
+            instance.superAdministradores = new ArrayList<>();
+            instance.productos = new ArrayList<>();
+            instance.planificacionActual = new PlanificadorA();
+        }
+        return instance;
+    }
+
 
 }
--- src/main/java/Business/Reserva.java
+++ src/main/java/Business/Reserva.java
@@ -14,7 +14,7 @@ public class Reserva {
     private Funcion funcion;
     private List<Asiento> asientos;
     private List<Producto> productos;
-    private int costoTotal;
+    private float costoTotal;
     private String codigoDeReserva;
 
 }
